ff2a033ce95f398940f54d40bbc162f4880908f6
Phil Haack <haacked@gmail.com>
2018-01-21 13:57:27 -0800

    Add the title to the log

--- test-github-webhook/index.js
+++ test-github-webhook/index.js
@@ -1,6 +1,6 @@
 // Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
 module.exports = function (context, data) {
-    context.log('GitHub Webhook triggered!', data.comment.body);
+    context.log('GitHub Webhook triggered!', data.comment.body + ' on issue ' + data.issue.title);
     context.res = { body: 'New GitHub Comment: ' + data.comment.body + ' on issue ' + data.issue.title };
     context.done();
 };
